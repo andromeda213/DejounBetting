f924db166a66939b8cc2a51971cdb535e544f317
unknown <meepwn.clone2@gmail.com>
2020-08-15 18:13:55 +0800

    update code

--- DejounSPA/src/app/_services/error.interceptor.ts
+++ DejounSPA/src/app/_services/error.interceptor.ts
@@ -16,34 +16,34 @@ export class ErrorInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-      return next.handle(req).pipe(
-          catchError(error => {
-            if(error.status === 401){
-                return throwError(error.statusText);
+    return next.handle(req).pipe(
+      catchError((error) => {
+        if (error.status === 401) {
+          return throwError(error.statusText);
+        }
+        if (error instanceof HttpErrorResponse) {
+          const applicationError = error.headers.get('Application-Error');
+          if (applicationError) {
+            return throwError(applicationError);
+          }
+          const serverError = error.error;
+          let modalStateErrors = '';
+          if (serverError.errors && typeof serverError.errors === 'object') {
+            for (const key in serverError.errors) {
+              if (serverError.errors[key]) {
+                modalStateErrors += serverError.errors[key] + '\n';
+              }
             }
-            if(error instanceof HttpErrorResponse){
-                const applicationError = error.headers.get('Application-Error');
-                if(applicationError){
-                    return throwError(applicationError);
-                }
-                const serverError = error.error;
-                let modalStateErrors = '';
-                if(serverError.errors && typeof serverError.errors === 'object'){
-                    for(const key in serverError.errors){
-                        if(serverError.errors[key]){
-                            modalStateErrors += serverError.errors[key] + '\n';
-                        }
-                    }
-                }
-                return throwError(modalStateErrors || serverError || 'Server Error');
-            }
-          })
-      )
+          }
+          return throwError(modalStateErrors || serverError || 'Server Error');
+        }
+      })
+    );
   }
 }
 
 export const ErrorInterceptorProvider = {
-    provide: HTTP_INTERCEPTORS,
-    useClass: ErrorInterceptor,
-    multi: true
-}
+  provide: HTTP_INTERCEPTORS,
+  useClass: ErrorInterceptor,
+  multi: true,
+};
