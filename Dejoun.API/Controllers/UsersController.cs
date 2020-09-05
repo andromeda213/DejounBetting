e4d37372dcde2eb10cae5322461a95d191b1177b
mmiclat@tigernethost.com <mmiclat@tigernethost.com>
2020-09-05 17:30:00 +0800

    adfadfasdf

--- Dejoun.API/Controllers/UsersController.cs
+++ Dejoun.API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
 using Dejoun.API.Data;
@@ -42,6 +43,23 @@ namespace Dejoun.API.Controllers
             var user = await _repo.GetUser(id);
             var userToReturn = _mapper.Map< UserForDetailedDto>(user);
             return Ok(userToReturn);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserForUpdateDto userForUpdate)
+        {
+            if (id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+
+            var userFromRepo = await _repo.GetUser(id);
+
+            _mapper.Map(userForUpdate, userFromRepo);
+
+
+            if(await _repo.SaveAll())
+                return NoContent();
+
+            throw new Exception($"Updating user {id} failed on save.");
         }
 
     }
